64850eee78c3f876f7b11fe015a02de9486910eb
ksdoit_ <komaldoneriya@gmail.com>
2026-01-30 08:21:03 +0530

    Update API_URL to include '/api' endpoint

--- front/my-react-app/src/App.js
+++ front/my-react-app/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'https://notecrypt-mdkz.onrender.com';
+const API_URL = 'https://notecrypt-mdkz.onrender.com/api';
 
 function App() {
   const [username, setUsername] = useState('');
